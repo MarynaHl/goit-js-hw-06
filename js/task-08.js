112941cb8fd59060c278e409bac5bd58e371be3a
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-07-11 15:51:48 +0100

    Завдання 8

--- js/task-08.js
+++ js/task-08.js
@@ -22,3 +22,20 @@
 // Виведи об'єкт із введеними даними в консоль і очисти
 //  значення полів форми методом reset.
 
+
+const formRef = document.querySelector('form.login-form');
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password =  formElements.password.value;
+    const formData = {email, password}; 
+    if (email && password) {
+        console.log(formData)
+    } 
+    else {
+        alert('Please, complete all fields')
+    }
+}
