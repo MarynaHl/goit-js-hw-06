addeb227495f47d3b49b9f8395341dd3cd13e1a4
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-07-11 15:31:36 +0100

    Завдання 7

--- js/task-07.js
+++ js/task-07.js
@@ -9,3 +9,10 @@
 // <span id="text">Abracadabra!</span>
 
 
+const fontSizeEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+
+fontSizeEl.addEventListener('input', onInputChange);
+
+function onInputChange({currentTarget}){
+    text.setAttribute('style', `font-size: ${currentTarget.value}px`);
