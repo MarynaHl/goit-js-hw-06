827cf9fa7f95d19da85ee1c1275c9d4d0774f38a
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-07-11 20:32:03 +0100

    Завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -14,6 +14,21 @@
 //     .padStart(6, 0)}`;
 // }
 
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+const changeColorBtn = document.querySelector('button.change-color');
+const body = document.querySelector('body');
+const nameColor = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  nameColor.textContent = color;
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
