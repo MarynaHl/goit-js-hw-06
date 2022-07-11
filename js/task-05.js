38a8b365cf1d56ba6d1e555a1eff701bd258cd57
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-07-11 14:56:01 +0100

    Завдання 5

--- js/task-05.js
+++ js/task-05.js
@@ -6,3 +6,14 @@
 // <input type="text" id="name-input" placeholder="Please enter your name" />
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
 
+const inputBtn = document.querySelector('input#name-input');
+const output = document.querySelector('span#name-output');
+
+inputBtn.addEventListener('input', onInputBtnInput);
+
+function onInputBtnInput(event) {
+  if (event.currentTarget.value === '') {
+    return (output.textContent = 'Anonymous');
+  }
+  output.textContent = event.currentTarget.value;
+}
