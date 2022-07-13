5d0e37dbdf83c0aaa881cdf3fa1842e1d440fc80
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-07-13 17:27:28 +0100

    fix

--- js/task-07.js
+++ js/task-07.js
@@ -1,27 +1,11 @@
 
-// Напиши скрипт, який реагує на зміну значення 
-// input#font-size-control (подія input) і змінює інлайн-стиль span#text, 
-// оновлюючи властивість font-size. В результаті, перетягуючи повзунок, 
-// буде змінюватися розмір тексту.
+const fontSizeEl = {
+    fontSizeControl: document.querySelector('#font-size-control'),
+    textRef: document.querySelector('#text')
+}
 
-// <input id="font-size-control" type="range" min="16" max="96" />
-// <br />
-// <span id="text">Abracadabra!</span>
+ref.fontSizeControl.addEventListener('input',changeFontSize)
 
-
-// const inputBtn = document.querySelector('input#font-size-control');
-// const textString = document.querySelector('span#text');
-
-//     inputBtn.addEventListener('input', onInputBtnChange);
-//     textString.style.fontSize = `${inputBtn.value}px`;
-// function onInputBtnChange(event) {
-//     textString.style.fontSize = `${inputBtn.value}px`;
-// }
-// 
-const fontSizeEl = document.querySelector('#font-size-control');
-const textEl = document.querySelector('#text');
-
-fontSizeEl.addEventListener('input', onInputChange);
-
-function onInputChange({currentTarget}){
-    text.setAttribute('style', `font-size: ${currentTarget.value}px`);
+function onInputChange(){
+    fontSizeEl.textRef.style.fontSize = fontSizeEl.fontSizeControl.value + 'px';
+}
