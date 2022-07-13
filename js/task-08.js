63eb391b0adaec2a0ab2f62c7cad2ee17d95fa41
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-07-13 17:56:26 +0100

    fix

--- js/task-08.js
+++ js/task-08.js
@@ -24,24 +24,21 @@
 
 const refs = {
     loginFormRef: document.querySelector('.login-form'),
- }
- 
- refs.loginFormRef.addEventListener('submit', processingLoginForm)
- 
- function processingLoginForm(event) {
+}
+
+refs.loginFormRef.addEventListener('submit', processingLoginForm)
+
+function processingLoginForm(event) {
     event.preventDefault();
     const formElementsRef = event.currentTarget.elements
     const mail = formElementsRef.email.value;
     const password = formElementsRef.password.value;
     if (mail == "" || password == "") {
-       alert('Всі поля треба заповнити')
+        alert('Всі поля треба заповнити')
     } else {
-       const formData = {
-          mail,
-          password
-       }
-       console.log(formData);
-          formElementsRef.email.value = '';
-          formElementsRef.password.value = '';
+        const formData = { mail, password } 
+        console.log(formData);
+        formElementsRef.email.value = '';
+        formElementsRef.password.value = '';
     }
 }
