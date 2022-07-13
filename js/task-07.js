e16e5243771d94ac105673664edca5164e987832
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-07-13 17:31:05 +0100

    fix

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,13 @@
 
-const fontSizeEl = {
+const ref = {
     fontSizeControl: document.querySelector('#font-size-control'),
     textRef: document.querySelector('#text')
-}
-
-ref.fontSizeControl.addEventListener('input',changeFontSize)
-
-function onInputChange(){
-    fontSizeEl.textRef.style.fontSize = fontSizeEl.fontSizeControl.value + 'px';
-}
+ }
+ 
+ ref.fontSizeControl.addEventListener('input',changeFontSize)
+ 
+ function changeFontSize() {
+    console.log(ref.fontSizeControl.value);
+    console.log(ref.textRef.style.fontSize);
+    ref.textRef.style.fontSize = ref.fontSizeControl.value + 'px';
+ }
